26f7f3bc1551393af43596a4b607884b0ce83d98
Ivan Volodartsev <untrupus@gmail.com>
2021-05-20 14:16:20 +0600

    create contacts list

--- src/controller.js
+++ src/controller.js
@@ -5,13 +5,35 @@ export default class Controller {
 
         view.login(this.login.bind(this));
         view.logout(this.logout.bind(this));
+        view.setUserList(this.getUsersList(), this.model.user, this.addUser.bind(this));
+        view.showChatWindow(this.getLogin());
     }
 
+    /** Login */
     login(name) {
         this.model.login(name);
+        this.view.clearUserList();
+        this.view.setUserList(this.getUsersList(), this.model.user);
     }
 
+    /** Logout */
     logout() {
         this.model.logout();
+    }
+
+    /** Get users list from local storage */
+    getUsersList() {
+        this.model.getUsers();
+        return this.model.users;
+    }
+
+    /** Get your name */
+    getLogin() {
+       return this.model.getLogin();
+    }
+
+    /** Add username to list */
+    addUser(name) {
+        this.model.addUser(name);
     }
 }
--- src/view.js
+++ src/view.js
@@ -1,26 +1,63 @@
 export default class View {
     constructor() {
-
+        this.chatWindow = document.getElementById('chatWindow');
+        this.loginBlock = document.getElementById('loginBlock');
+        this.loginBtn = document.getElementById('loginBtn');
+        this.logoutBtn = document.getElementById('logoutBtn');
+        this.userName = document.getElementById('loginInput');
+        this.userList = document.getElementById('usersList')
     }
 
+    /** Add action on login button */
     login(handler) {
-        const userName = document.getElementById('loginInput');
-        document.getElementById('loginBtn').addEventListener('click', () => {
-            if (userName.value !== '') {
-                console.log(userName.value);
-                handler(userName.value);
-                document.getElementById('loginBlock').className = 'hide';
-                document.getElementById('chatWindow').className = 'chatWindow';
-                userName.value = '';
+        this.loginBtn.addEventListener('click', () => {
+            if (this.userName.value !== '') {
+                handler(this.userName.value);
+                this.loginBlock.className = 'hide';
+                this.chatWindow.className = 'chatWindow';
+                this.userName.value = '';
             }
         });
     }
 
+    /** Add action on logout button */
     logout(handler) {
-        document.getElementById('logoutBtn').addEventListener('click', () => {
+        this.logoutBtn.addEventListener('click', () => {
             handler();
-            document.getElementById('chatWindow').className = 'hide';
-            document.getElementById('loginBlock').className = 'login';
+            this.chatWindow.className = 'hide';
+            this.loginBlock.className = 'login';
         });
     }
+
+    /** Clear users list  */
+    clearUserList() {
+        this.userList.innerHTML = '';
+    }
+
+    /** Render users in users list */
+    setUserList(userList, yourAccount, handler) {
+        if (userList !== null) {
+            userList.forEach(user => {
+                const newUser = document.createElement('li');
+                newUser.className = 'item';
+                newUser.id = user
+                newUser.innerHTML = user;
+                if (user !== yourAccount) {
+                    this.userList.append(newUser);
+                }
+
+                handler ? handler(user) : null;
+            });
+        }
+    }
+
+    /** Set the visibility of chat window */
+    showChatWindow(value) {
+        if (value) {
+            this.loginBlock.className = 'hide';
+            this.chatWindow.className = 'chatWindow';
+        }
+    }
+
+
 }
